52eb33b00dbb99a66672d07fd4e7269a05ae8d22
CHARAN422176 <422176@student.nitandhra.ac.in>
2025-09-09 16:15:11 +0530

    test seanet loop changed

--- test_SeaNet.py
+++ test_SeaNet.py
@@ -36,6 +36,22 @@ for dataset in test_datasets:
     gt_root = '/kaggle/input/eorssd/test-labels/'
     test_loader = test_dataset(image_root, gt_root, opt.testsize)
     time_sum = 0
+    # for i in range(test_loader.size):
+    #     image, gt, name = test_loader.load_data()
+    #     gt = np.asarray(gt, np.float32)
+    #     gt /= (gt.max() + 1e-8)
+    #     image = image.cuda()
+    #     time_start = time.time()
+    #     res, s34, s5, s12_sig, s34_sig, s5_sig, edge1, edge2 = model(image)
+    #     time_end = time.time()
+    #     time_sum = time_sum+(time_end-time_start)
+    #     res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
+    #     res = res.sigmoid().data.cpu().numpy().squeeze()
+    #     res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+    #     imageio.imsave(save_path+name, res)
+    #     if i == test_loader.size-1:
+    #         print('Running time {:.5f}'.format(time_sum/test_loader.size))
+    #         print('FPS {:.5f}'.format(test_loader.size / time_sum))
     for i in range(test_loader.size):
         image, gt, name = test_loader.load_data()
         gt = np.asarray(gt, np.float32)
@@ -48,7 +64,11 @@ for dataset in test_datasets:
         res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
         res = res.sigmoid().data.cpu().numpy().squeeze()
         res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-        imageio.imsave(save_path+name, res)
+
+        # ✅ fix: convert to uint8 before saving
+        res = (res * 255).astype(np.uint8)
+        imageio.imsave(save_path + name, res)
+
         if i == test_loader.size-1:
             print('Running time {:.5f}'.format(time_sum/test_loader.size))
             print('FPS {:.5f}'.format(test_loader.size / time_sum))
